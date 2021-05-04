ba0f38e9e01c995844954193d4884b1a9c8ab0d3
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2021-05-04 15:27:05 +0700

    quick fix

--- client-web/src/pages/Timeoff/MyTimeoff/index.tsx
+++ client-web/src/pages/Timeoff/MyTimeoff/index.tsx
@@ -254,8 +254,8 @@ export const Timeoff: React.FC = () => {
         onFinish={async (value) => {
           const start_date = moment(value.off_days![0]);
           const end_date = moment(value.off_days![1]);
-          start_date.set({ hours: 23, minutes: 59 });
-          end_date.set({ hours: 0, minutes: 0 });
+          start_date.set({ hours: 0, minutes: 0 });
+          end_date.set({ hours: 23, minutes: 59 });
           const record = {
             ...value,
             start_date,
