bb10d949834734b3b4a9c50941f0265c628fc12a
Nguyễn Huỳnh Lợi <loia5tqd001@gmail.com>
2021-05-06 10:01:12 +0700

    Remove readonly columns Card in GeneralInformation tab

--- client-web/src/pages/Payroll/Template/Detail/GeneralInformation.tsx
+++ client-web/src/pages/Payroll/Template/Detail/GeneralInformation.tsx
@@ -1,12 +1,7 @@
-import { readPayrollTemplate, updatePayrollTemplate } from '@/services/payroll.template';
+import { updatePayrollTemplate } from '@/services/payroll.template';
 import ProForm, { ProFormText } from '@ant-design/pro-form';
-import ProList from '@ant-design/pro-list';
-import { Card, Space, Tag, message } from 'antd';
-import { useForm } from 'antd/lib/form/Form';
-import faker from 'faker';
-import React, { useState } from 'react';
-import { useEffect } from 'react';
-import { useParams } from 'umi';
+import { Card, message } from 'antd';
+import React from 'react';
 
 type Props = {
   payrollTemplate: API.PayrollTemplate | undefined;
@@ -14,25 +9,6 @@ type Props = {
 
 export const GeneralInformation: React.FC<Props> = (props) => {
   const { payrollTemplate } = props;
-
-  // const dataSource = [
-  //   {
-  //     name: 'Work day',
-  //     desc: 'Description: number of days of working',
-  //   },
-  //   {
-  //     name: 'Career change day',
-  //     desc: 'Description: no description',
-  //   },
-  //   {
-  //     name: 'Old stage salary',
-  //     desc: 'Description: the previous position salary',
-  //   },
-  //   {
-  //     name: 'Salary',
-  //     desc: 'Description: The current salary',
-  //   },
-  // ];
 
   return (
     <div style={{ display: 'grid', gap: 24 }}>
@@ -58,29 +34,10 @@ export const GeneralInformation: React.FC<Props> = (props) => {
         >
           <ProForm.Group>
             <ProFormText width="sm" rules={[{ required: true }]} name="name" label="Name" />
-            {/* <ProFormText
-              width="sm"
-              rules={[{ required: true }]}
-              name="first_name"
-              label="Department"
-            />
-            <ProFormText width="sm" rules={[{ required: true }]} name="last_name" label="Creator" />
-            <ProFormText
-              width="sm"
-              rules={[{ required: true }]}
-              name="last_name"
-              label="Create time"
-            />
-            <ProFormText
-              width="sm"
-              rules={[{ required: true }]}
-              name="last_name"
-              label="Workflow"
-            /> */}
           </ProForm.Group>
         </ProForm>
       </Card>
-      <Card title="Columns">
+      {/* <Card title="Columns">
         <ProList<any>
           rowKey="name"
           headerTitle={false}
@@ -106,7 +63,7 @@ export const GeneralInformation: React.FC<Props> = (props) => {
             },
           }}
         />
-      </Card>
+      </Card> */}
     </div>
   );
 };
